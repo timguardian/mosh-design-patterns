11e0f0e6fc00d7f4ff44a7e0112459f0327c430b
Duisengaliyev Temirlan <duisengaliyev.temirlan@gmail.com>
2022-06-30 23:35:54 +0600

    Observer pattern

---
+++ observer/Main.java
@@ -0,0 +1,16 @@
+package observer;
+
+public class Main {
+    public static void main(String[] args) {
+        var dataSource = new DataSource();
+        var sheet1 = new Spreadsheet();
+        var sheet2 = new Spreadsheet();
+        var chart = new Chart();
+
+        dataSource.addObserver(sheet1);
+        dataSource.addObserver(sheet2);
+        dataSource.addObserver(chart);
+
+        dataSource.setValue(1);
+    }
+}
---
+++ observer/Spreadsheet.java
@@ -0,0 +1,8 @@
+package observer;
+
+public class Spreadsheet implements Observer{
+    @Override
+    public void update(int value) {
+        System.out.println("Spreadsheet got notified: " + value);
+    }
+}
